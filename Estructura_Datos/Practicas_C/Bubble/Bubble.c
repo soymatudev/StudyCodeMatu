6997e2ffd3b99e12d1f9c32af498d7259639aef3
SoyMatudev <juanjesusgarciamaturana@gmail.com>
2024-08-02 20:36:02 -0600

    Correccion de Bubble

--- Estructura_Datos/Practicas_C/Bubble/Bubble.c
+++ Estructura_Datos/Practicas_C/Bubble/Bubble.c
@@ -6,14 +6,17 @@ int main()
     // Declaracion de variables y del arreglo
     int coleccion[] = {5, 10, 2, 12, 1};
     int aux = 0;
+    int n = sizeof(coleccion) / sizeof(coleccion[0]);
 
     // Ordenar la coleccion
-    /* El primer hace que se repita el proceso el numero de veces
-    segun el tamaño de la coleccion */
-    for (int i = 0; i < 5; i++)
+    // El primer hace que se repita el proceso el numero de veces segun el tamaño de la coleccion
+    // El -1 es para que no se compare el ultimo elemento con el siguiente ya que no hay siguiente
+    for (int i = 0; i < n - 1; i++)
     {
-        /* El segundo for hace que se compare el elemento actual con el siguiente*/
-        for (int j = 0; j < 5; j++)
+        //  El segundo for hace que se compare el elemento actual con el siguiente
+        //  El -1 es para que no se compare el ultimo elemento con el siguiente ya que no hay siguiente
+        //  El -i es para que no se compare con los elementos que ya estan ordenados al final de la coleccion
+        for (int j = 0; j < n - 1 - i; j++)
         {
             if (coleccion[j] > coleccion[j + 1])
             {
@@ -26,7 +29,7 @@ int main()
 
     // Imprimir la coleccion ordenada
     printf("\nColeccion ordenada: \n");
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < n; i++)
     {
         printf("%d \n", coleccion[i]);
     }
